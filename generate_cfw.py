335bfa9679cbfdc4c1e2115604a5d4bde18fc7d9
mark0smith <noreply@github.com>
2020-05-01 10:29:30 +0800

    修改优化

--- generate_cfw.py
+++ generate_cfw.py
@@ -2,6 +2,9 @@ import requests
 import json
 import yaml
 import time
+import concurrent.futures
+import random
+
 
 def get_proxylist(filename = 'proxy.list'):
     raw_url = "https://github.com/fate0/proxylist/raw/master/proxy.list"
@@ -9,6 +12,46 @@ def get_proxylist(filename = 'proxy.list'):
     with open(filename,'wb') as fw:
         for iter in req.iter_content(1024):
             fw.write(iter)
+
+def check_single_proxy(proxy):
+    proxies = {
+        "http":f"http://{proxy['host']}:{proxy['port']}",
+        "https":f"https://{proxy['host']}:{proxy['port']}",
+    }
+    try:
+        if proxy['type'] == "https":
+            url = "https://www.baidu.com/robots.txt"
+        else:
+            url = "http://baidu.com/robots.txt"
+        req = requests.get(url,proxies=proxies,timeout=3)
+        assert "Baiduspider" in req.text
+        assert req.status_code == 200
+        print(f"[+] Valid Proxy [{proxy['country']}] ==> {proxy['type']}://{proxy['host']}:{proxy['port']}")
+        return True
+    except Exception as e:
+        pass
+
+    return False
+
+
+def check_valid(proxy_list):
+    result = []
+    # proxy_list = random.choices(proxy_list,k=min(100,len(proxy_list)))
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+        future_to_url = {executor.submit(check_single_proxy, proxy): proxy for proxy in proxy_list}
+        for future in concurrent.futures.as_completed(future_to_url):
+            proxy = future_to_url[future]
+            try:
+                data = future.result()
+                if data == True:
+                    result.append(proxy)
+            except Exception as exc:
+                pass
+
+    return result
+
+            
 
 def generate_config(filename = 'proxy.list'):
     proxy_list = []
@@ -18,6 +61,8 @@ def generate_config(filename = 'proxy.list'):
         for line in fr.readlines():
             proxy = json.loads(line.strip())
             proxy_list.append(proxy)
+    
+    proxy_list = check_valid(proxy_list)
     
     output['Proxy'] = []
     output['Proxy Group'] = [] 
@@ -31,7 +76,7 @@ def generate_config(filename = 'proxy.list'):
     for proxy in proxy_list:
         index += 1
         p = {}
-        p['name'] = f"{index}"
+        p['name'] = f"{index} [{proxy['country']}] ==> {proxy['type']}://{proxy['host']}:{proxy['port']}"
         p['type'] = "http"
         p['server'] = proxy['host']
         p['port'] = proxy['port']
@@ -41,18 +86,18 @@ def generate_config(filename = 'proxy.list'):
             p['tls']= True
             p['skip-cert-verify']= True
         if proxy['country'] == "CN":
-            proxy_group_cn.append(str(index))
+            proxy_group_cn.append(p['name'])
         elif proxy['country'] == "US":
-            proxy_group_us.append(str(index))
+            proxy_group_us.append(p['name'])
         else:
-            proxy_group_others.append(str(index))
+            proxy_group_others.append(p['name'])
 
-        proxy_group_all.append(str(index))
+        # proxy_group_all.append(p['name'])
         output['Proxy'].append(p)
     
     g = {
         'name':'Proxy',
-        'proxies': proxy_group_all,
+        'proxies': ["CN Proxy","US Proxy","Others Proxy"],
         'type':"select",
         # "url":"http://www.gstatic.com/generate_204",
         "interval":600
@@ -78,10 +123,11 @@ def generate_config(filename = 'proxy.list'):
         "url":"http://www.gstatic.com/generate_204",
         "interval":600
     }
+    output['Proxy Group'].append(g)
     output['Proxy Group'].append(proxy_cn)
     output['Proxy Group'].append(proxy_us)
     output['Proxy Group'].append(proxy_others)
-    output['Proxy Group'].append(g)
+    
     # output['Rules'] = ['MATCH,CN Proxy']
 
     with open("temp_config.yml",'w') as fw:
